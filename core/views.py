77e0823f701acf629cc0bb759521f27165da6006
Ivek <ivekdelfin@gmail.com>
2019-07-19 20:49:29 -0500

    adapto template portada

--- core/views.py
+++ core/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render, HttpResponse
-http_base = """ 
-            <h1>Página de práctica</h1>
-            <ul>
-                <li><a href='/'>Portada</a></li>
-                <li><a href='/about'>Acerca de</a></li>
-                <li><a href='/portfolio'>Portafolio</a></li>
-                <li><a href='/contact'>Contacto</a></li>
-            </ul>"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse(http_base + """ <h2>les manda saludos al 
-                        equipo ALFA el Ingeniero Ivek</h2>""")
+    return render(request,'core/home.html')
 
 def about(request):
-  return HttpResponse(http_base + """<h2>Acerca de mi</h2> 
-                        <h2>Soy Ingeniero Ivek amo la tecnologia
-                        y me encanta Python</h2>""")
+    return render(request,'core/about.html')
 
 def portfolio(request):
-    return HttpResponse(http_base + """<h2>Portafolio</h2> 
-                        <h2>He realizado pocos 
-                        proyectos pero bonitos</h2>""")
+    return render(request,'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(http_base + """ <h2>Contacto</h2>
-                          <h2>mi correo de contacto es
-                          ivekdelfin@gmail.com  </h2>""")
+     return render(request,'core/contact.html')
