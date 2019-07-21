f518578b47e13a92bf1a800e2cb8817562e85f3d
Ivek <ivekdelfin@gmail.com>
2019-07-21 00:31:53 -0500

    CONFIGURADO EL ADMIN Y SOPORTE DE MANEJO DE IMAGEN

--- perfil_personal/urls.py
+++ perfil_personal/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
-
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -24,3 +24,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
