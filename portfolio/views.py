8164ec49ea8796a1b8c8748f1ccce29f41a3bd51
Ivek <ivekdelfin@gmail.com>
2019-07-21 02:10:15 -0500

    webapp finalizada editar informacion de portafolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def portfolio(request):
-    return render(request,'portfolio/portfolio.html')
+    modelo = Project.objects.all()
+    return render(request,'portfolio/portfolio.html',{'modelo':modelo})
